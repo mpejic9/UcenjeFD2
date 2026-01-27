6a54d5cd83742a6a6e9d5d8fea24501c2ff6c843
Matko Pejić <pejicmatko9@gmail.com>
2026-01-27 20:55:51 +0100

    JS Hello World

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,17 @@
+console.info('Hello world 6');
+console.table({ime:"Pero",prezime:"Perić"});
+
+document.getElementById('ispis').innerText='Hello World 7';
+document.getElementById('ispis').style.color='red';
+
+document.getElementById('ue').innerHTML='<a href="#">Hello world 8 </a>'
+
+console.log('Osnovna škola "Ivana Filipovića"');
+
+console.log('Mc\'Donalds')
+
+console.log(`
+    Ovo je moj
+    tekst
+    kroz više redova
+    `)
